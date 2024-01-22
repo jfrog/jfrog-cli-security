676643178b593bb0d597393ed4920457d88dee41
Or Zinger <orz@jfrog.com>
2024-01-22 10:38:02 +0200

    Cr fix

--- utils/analyzermanager.go
+++ utils/analyzermanager.go
@@ -36,6 +36,7 @@ const (
 	unsupportedOsExitCode                     = 55
 	ErrFailedScannerRun                       = "failed to run %s scan. Exit code received: %s"
 	jfrogCliAnalyzerManagerVersionEnvVariable = "JFROG_CLI_ANALYZER_MANAGER_VERSION"
+	JF_MSI                                    = "JF_MSI"
 )
 
 type ApplicabilityStatus string
@@ -91,7 +92,7 @@ func (am *AnalyzerManager) ExecWithOutputFile(configFile, scanCommand, workingDi
 		return
 	}
 	var cmd *exec.Cmd
-	multiScanId := os.Getenv("JF_MSI")
+	multiScanId := os.Getenv(JF_MSI)
 	if len(outputFile) > 0 {
 		log.Debug("Executing", am.AnalyzerManagerFullPath, scanCommand, configFile, outputFile, multiScanId)
 		cmd = exec.Command(am.AnalyzerManagerFullPath, scanCommand, configFile, outputFile)
