24a741921741401c7ff94f49611bd08f9bb1ef77
dortam888 <dortam888@gmail.com>
2024-08-04 00:30:36 +0300

    change function name in tests

--- commands/scan/buildscan_test.go
+++ commands/scan/buildscan_test.go
@@ -9,20 +9,20 @@ import (
 func TestTrimUrlFunc(t *testing.T) {
 	// Test empty string
 	emptyUrl := ""
-	url, endpoint, err := trimBuildScanResultUrl(emptyUrl)
+	url, endpoint, err := trimUrl(emptyUrl)
 	assert.NoError(t, err)
 	assert.True(t, url == "")
 	assert.True(t, endpoint == "")
 
 	// Test good url trim
 	goodUrl := "http://dort.jfrog.io/xray/random/api"
-	url, endpoint, err = trimBuildScanResultUrl(goodUrl)
+	url, endpoint, err = trimUrl(goodUrl)
 	assert.NoError(t, err)
 	assert.True(t, url == "http://dort.jfrog.io/")
 	assert.True(t, endpoint == "xray/random/api")
 
 	// Test bad url
 	badUrl := "http://dort.jfrog io/xray/random/api"
-	_, _, err = trimBuildScanResultUrl(badUrl)
+	_, _, err = trimUrl(badUrl)
 	assert.NotNil(t, err)
 }
