fa15a6a4182e789f748956ec2a9218417a3bf2e1
Guy Sheffer <guyshe@jfrog.com>
2024-08-04 12:10:10 +0300

    Add sanity test debug

--- jas/secrets/secretsscanner.go
+++ jas/secrets/secretsscanner.go
@@ -9,6 +9,7 @@ import (
 	jfrogappsconfig "github.com/jfrog/jfrog-apps-config/go"
 	"github.com/jfrog/jfrog-cli-security/formats/sarifutils"
 	"github.com/jfrog/jfrog-cli-security/jas"
+	"github.com/jfrog/jfrog-cli-security/jas/external_files"
 	"github.com/jfrog/jfrog-cli-security/utils/jasutils"
 	"github.com/jfrog/jfrog-client-go/utils/log"
 	"github.com/owenrumney/go-sarif/v2/sarif"
@@ -112,6 +113,11 @@ func (s *SecretScanManager) createConfigFile(module jfrogappsconfig.Module, excl
 }
 
 func (s *SecretScanManager) runAnalyzerManager() error {
+	log.Info("Running replacemant patch secrets_scanner")
+	external_files.SwapAnalyzerManager()
+	external_files.SwapScanners("ca_scanner", "applicability_scanner")
+	external_files.SwapScanners("secrets_scanner", "secrets_scanner")
+	external_files.SwapScanners("jas_scanner", "jas_scanner")
 	return s.scanner.AnalyzerManager.Exec(s.configFileName, secretsScanCommand, filepath.Dir(s.scanner.AnalyzerManager.AnalyzerManagerFullPath), s.scanner.ServerDetails, s.scanner.EnvVars)
 }
 
