34dd5101d9f14bb50246b7a477034d1d7796d2e1
dortam888 <dortam888@gmail.com>
2024-08-05 18:02:47 +0300

    add test for get url function

--- commands/scan/buildscan_test.go
+++ commands/scan/buildscan_test.go
@@ -3,6 +3,7 @@ package scan
 import (
 	"testing"
 
+	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -25,4 +26,26 @@ func TestTrimUrlFunc(t *testing.T) {
 	badUrl := "http://dort.jfrog io/xray/random/api"
 	_, _, err = trimUrl(badUrl)
 	assert.NotNil(t, err)
+}
+
+func TestGetActualUrl(t *testing.T) {
+	buildScanCommand := NewBuildScanCommand()
+	testServerDetails := &config.ServerDetails{
+		Url:            "http://dort.jfrog.io/",
+		ArtifactoryUrl: "http://dort.jfrog.io/artifactory",
+		XrayUrl:        "http://dort.jfrog.io/xray",
+	}
+
+	// test when JFrog URL is provided
+	buildScanCommand.SetServerDetails(testServerDetails)
+	expectedUrl := "http://dort.jfrog.io/"
+	actualUrl, err := getActualUrl(*buildScanCommand.serverDetails)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedUrl, actualUrl)
+
+	// test when only Artifactory URL and Xray URL are provided and URL is empty
+	buildScanCommand.serverDetails.Url = ""
+	actualUrl, err = getActualUrl(*buildScanCommand.serverDetails)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedUrl, actualUrl)
 }
