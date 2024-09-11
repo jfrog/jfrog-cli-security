b68ecee63beaac784dfb357b9cf842ccc5c57c85
attiasas <assafattias93@gmail.com>
2024-09-11 17:42:42 +0300

    Try Revert to AM v1.8.13

--- jas/analyzermanager.go
+++ jas/analyzermanager.go
@@ -24,7 +24,7 @@ import (
 const (
 	ApplicabilityFeatureId                    = "contextual_analysis"
 	AnalyzerManagerZipName                    = "analyzerManager.zip"
-	defaultAnalyzerManagerVersion             = "1.8.14"
+	defaultAnalyzerManagerVersion             = "1.8.13"
 	analyzerManagerDownloadPath               = "xsc-gen-exe-analyzer-manager-local/v1"
 	analyzerManagerDirName                    = "analyzerManager"
 	analyzerManagerExecutableName             = "analyzerManager"
