7ecb1be3e43b169fdadbfd2fdde8bd054f7fae09
attiasas <assafattias93@gmail.com>
2024-10-07 09:41:33 +0300

    update AM to 1.9.4

--- jas/analyzermanager.go
+++ jas/analyzermanager.go
@@ -24,7 +24,7 @@ import (
 const (
 	ApplicabilityFeatureId                    = "contextual_analysis"
 	AnalyzerManagerZipName                    = "analyzerManager.zip"
-	defaultAnalyzerManagerVersion             = "1.9.3"
+	defaultAnalyzerManagerVersion             = "1.9.4"
 	analyzerManagerDownloadPath               = "xsc-gen-exe-analyzer-manager-local/v1"
 	analyzerManagerDirName                    = "analyzerManager"
 	analyzerManagerExecutableName             = "analyzerManager"
