391927e60073ba5bd0f4b3a03dd4e310882ecf4b
barv <barv@jfrog.com>
2024-11-13 16:04:31 +0200

    cocoapods-audit

--- commands/audit/sca/cocoapods/cocoapods_test.go
+++ commands/audit/sca/cocoapods/cocoapods_test.go
@@ -39,7 +39,6 @@ func TestBuildCocoapodsDependencyList(t *testing.T) {
 		techutils.Cocoapods.GetPackageTypeId() + "GoogleSignIn:6.2.4",
 		techutils.Cocoapods.GetPackageTypeId() + "GTMAppAuth:1.3.1",
 		techutils.Cocoapods.GetPackageTypeId() + "GTMSessionFetcher:2.3.0",
-		techutils.Cocoapods.GetPackageTypeId() + "nanopb:0.4.1",
 		techutils.Cocoapods.GetPackageTypeId() + packageInfo,
 	}
 
