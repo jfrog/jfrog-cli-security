68f608f509d8f4dc0afd87b468bc479784a33858
Eran Turgeman <erant@jfrog.com>
2024-11-24 17:58:13 +0200

    fixing breaks from 'go vet ./..' . TestDetectScansToPreform requires fix

--- utils/xsc/analyticsmetrics_test.go
+++ utils/xsc/analyticsmetrics_test.go
@@ -117,16 +117,16 @@ func getDummyContentForGeneralEvent(withJas, withErr bool) *results.SecurityComm
 	scanResults.NewScaScanResults(services.ScanResponse{Vulnerabilities: vulnerabilities})
 
 	if withJas {
-		scanResults.JasResults.ApplicabilityScanResults = []*sarif.Run{sarifutils.CreateRunWithDummyResults(sarifutils.CreateDummyPassingResult("applic_CVE-123"))}
-		scanResults.JasResults.SecretsScanResults = []*sarif.Run{
+		scanResults.JasResultsNew.JasVulnerabilities.ApplicabilityScanResults = []*sarif.Run{sarifutils.CreateRunWithDummyResults(sarifutils.CreateDummyPassingResult("applic_CVE-123"))}
+		scanResults.JasResultsNew.JasVulnerabilities.SecretsScanResults = []*sarif.Run{
 			sarifutils.CreateRunWithDummyResults(sarifutils.CreateResultWithLocations("", "", "note", sarifutils.CreateLocation("", 0, 0, 0, 0, ""))),
 			sarifutils.CreateRunWithDummyResults(sarifutils.CreateResultWithLocations("", "", "note", sarifutils.CreateLocation("", 1, 1, 1, 1, ""))),
 		}
-		scanResults.JasResults.IacScanResults = []*sarif.Run{
+		scanResults.JasResultsNew.JasVulnerabilities.IacScanResults = []*sarif.Run{
 			sarifutils.CreateRunWithDummyResults(sarifutils.CreateResultWithLocations("", "", "note", sarifutils.CreateLocation("", 0, 0, 0, 0, ""))),
 			sarifutils.CreateRunWithDummyResults(sarifutils.CreateResultWithLocations("", "", "note", sarifutils.CreateLocation("", 1, 1, 1, 1, ""))),
 		}
-		scanResults.JasResults.SastScanResults = []*sarif.Run{
+		scanResults.JasResultsNew.JasVulnerabilities.SastScanResults = []*sarif.Run{
 			sarifutils.CreateRunWithDummyResults(sarifutils.CreateResultWithLocations("", "", "note", sarifutils.CreateLocation("", 0, 0, 0, 0, ""))),
 			sarifutils.CreateRunWithDummyResults(sarifutils.CreateResultWithLocations("", "", "note", sarifutils.CreateLocation("", 1, 1, 1, 1, ""))),
 		}
