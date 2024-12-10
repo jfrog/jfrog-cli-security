a9094dc2f3717ce4efdc2aa8732165e634d5924e
Eran Turgeman <erant@jfrog.com>
2024-12-10 09:48:17 +0200

    added new GetConfigProfileByUrl func and changed name GetConfigProfile -> GetConfigProfileByUrl

--- utils/xsc/configprofile.go
+++ utils/xsc/configprofile.go
@@ -8,18 +8,36 @@ import (
 	"github.com/jfrog/jfrog-client-go/xsc/services"
 )
 
-func GetConfigProfile(xrayVersion, xscVersion string, serverDetails *config.ServerDetails, profileName string) (*services.ConfigProfile, error) {
+func GetConfigProfileByName(xrayVersion, xscVersion string, serverDetails *config.ServerDetails, profileName string) (*services.ConfigProfile, error) {
+	// TODO eran do we need to change the minimal version verification to check Xray version and not Xsc version?
 	if err := clientutils.ValidateMinimumVersion(clientutils.Xsc, xscVersion, services.ConfigProfileMinXscVersion); err != nil {
-		log.Info("Minimal Xsc version required to utilize config profile is '%s'. All configurations will be induced from provided Env vars and files")
+		log.Info(fmt.Sprintf("Minimal Xsc version required to utilize config profile by url is '%s'. All configurations will be induced from provided Env vars and files", services.ConfigProfileByUrlMinXrayVersion))
 		return nil, err
 	}
 	xscService, err := CreateXscService(xrayVersion, serverDetails)
 	if err != nil {
 		return nil, err
 	}
-	configProfile, err := xscService.GetConfigProfile(profileName)
+	configProfile, err := xscService.GetConfigProfileByName(profileName)
 	if err != nil {
 		err = fmt.Errorf("failed to get config profile '%s': %q", profileName, err)
+	}
+	return configProfile, err
+}
+
+// TODO eran write test to this func
+func GetConfigProfileByUrl(xrayVersion string, serverDetails *config.ServerDetails) (*services.ConfigProfile, error) {
+	if err := clientutils.ValidateMinimumVersion(clientutils.Xray, xrayVersion, services.ConfigProfileByUrlMinXrayVersion); err != nil {
+		log.Info(fmt.Sprintf("Minimal Xray version required to utilize config profile by url is '%s'. All configurations will be induced from provided Env vars and files", services.ConfigProfileByUrlMinXrayVersion))
+		return nil, err
+	}
+	xscService, err := CreateXscService(xrayVersion, serverDetails)
+	if err != nil {
+		return nil, err
+	}
+	configProfile, err := xscService.GetConfigProfileByUrl(serverDetails.Url)
+	if err != nil {
+		err = fmt.Errorf("failed to get config profile for url '%s': %q", serverDetails.Url, err)
 	}
 	return configProfile, err
 }
