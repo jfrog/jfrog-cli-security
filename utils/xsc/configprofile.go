16200e469ddb8d8db867b4c5fabe3ceee55dcf94
Eran Turgeman <erant@jfrog.com>
2024-12-10 16:21:22 +0200

    improved service version check in GetConfigProfileByName to separate the new/old Xray cases

--- utils/xsc/configprofile.go
+++ utils/xsc/configprofile.go
@@ -6,13 +6,16 @@ import (
 	clientutils "github.com/jfrog/jfrog-client-go/utils"
 	"github.com/jfrog/jfrog-client-go/utils/log"
 	"github.com/jfrog/jfrog-client-go/xsc/services"
+	"github.com/jfrog/jfrog-client-go/xsc/services/utils"
 )
 
 func GetConfigProfileByName(xrayVersion, xscVersion string, serverDetails *config.ServerDetails, profileName string) (*services.ConfigProfile, error) {
-	// TODO eran do we need to change the minimal version verification to check Xray version and not Xsc version?
-	if err := clientutils.ValidateMinimumVersion(clientutils.Xsc, xscVersion, services.ConfigProfileMinXscVersion); err != nil {
-		log.Info(fmt.Sprintf("Minimal Xsc version required to utilize config profile by url is '%s'. All configurations will be induced from provided Env vars and files", services.ConfigProfileByUrlMinXrayVersion))
-		return nil, err
+	if !utils.IsXscXrayInnerService(xrayVersion) {
+		// We need to validate xsc version only if we use a Xray version prior to Xsc migration
+		if err := clientutils.ValidateMinimumVersion(clientutils.Xsc, xscVersion, services.ConfigProfileMinXscVersion); err != nil {
+			log.Info(fmt.Sprintf("Minimal Xsc version required to utilize config profile by url is '%s'. All configurations will be induced from provided Env vars and files", services.ConfigProfileByUrlMinXrayVersion))
+			return nil, err
+		}
 	}
 	xscService, err := CreateXscService(xrayVersion, serverDetails)
 	if err != nil {
