29015663f20421ffc6d2a942cc63f2de3e513980
Eran Turgeman <erant@jfrog.com>
2024-12-12 17:57:42 +0200

    fixed incorrectly passed repo url. fetching repo url by git client

--- utils/xsc/configprofile.go
+++ utils/xsc/configprofile.go
@@ -1,7 +1,9 @@
 package xsc
 
 import (
+	"context"
 	"fmt"
+	"github.com/jfrog/froggit-go/vcsclient"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
 	clientutils "github.com/jfrog/jfrog-client-go/utils"
 	"github.com/jfrog/jfrog-client-go/utils/log"
@@ -28,7 +30,7 @@ func GetConfigProfileByName(xrayVersion, xscVersion string, serverDetails *confi
 	return configProfile, err
 }
 
-func GetConfigProfileByUrl(xrayVersion string, serverDetails *config.ServerDetails) (*services.ConfigProfile, error) {
+func GetConfigProfileByUrl(xrayVersion string, serverDetails *config.ServerDetails, gitClient vcsclient.VcsClient, repoOwner string, repoName string) (*services.ConfigProfile, error) {
 	if err := clientutils.ValidateMinimumVersion(clientutils.Xray, xrayVersion, services.ConfigProfileByUrlMinXrayVersion); err != nil {
 		log.Info(fmt.Sprintf("Minimal Xray version required to utilize config profile by url is '%s'. All configurations will be induced from provided Env vars and files", services.ConfigProfileByUrlMinXrayVersion))
 		return nil, err
@@ -37,7 +39,15 @@ func GetConfigProfileByUrl(xrayVersion string, serverDetails *config.ServerDetai
 	if err != nil {
 		return nil, err
 	}
-	configProfile, err := xscService.GetConfigProfileByUrl(serverDetails.Url)
+
+	// TODO eran this is the latest addition I commited in this service, fix the test for this func
+	// Getting repository's url
+	repositoryInfo, err := gitClient.GetRepositoryInfo(context.Background(), repoOwner, repoName)
+	if err != nil {
+		return nil, err
+	}
+
+	configProfile, err := xscService.GetConfigProfileByUrl(repositoryInfo.CloneInfo.HTTP)
 	if err != nil {
 		err = fmt.Errorf("failed to get config profile for url '%s': %q", serverDetails.Url, err)
 	}
