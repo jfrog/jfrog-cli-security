d764c71ec35bd594412a481504d18e4bfbf15deb
attiasas <assafattias93@gmail.com>
2024-12-26 16:59:20 +0300

    Escape Job-Id for url

--- utils/results/conversion/sarifparser/sarifparser.go
+++ utils/results/conversion/sarifparser/sarifparser.go
@@ -2,6 +2,7 @@ package sarifparser
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -775,7 +776,7 @@ func getAnalyticsHiddenPixel(baseUrl string, resultOfSubScan utils.SubScanType) 
 	return fmt.Sprintf(
 		"![](%sui/api/v1/u?s=1&m=2&job_id=%s&run_id=%s&git_repo=%s&type=%s)",
 		baseUrl,
-		jobId,
+		url.PathEscape(jobId),
 		runId,
 		gitRepo,
 		resultOfSubScan.String(),
