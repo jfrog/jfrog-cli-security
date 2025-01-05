8d3ac07e69737e21b9c7f9d66bb820d1638e72fb
attiasas <assafattias93@gmail.com>
2025-01-05 18:12:00 +0300

    add new s m attributes

--- utils/results/output/securityJobSummary.go
+++ utils/results/output/securityJobSummary.go
@@ -549,7 +549,8 @@ func addAnalyticsQueryParamsIfNeeded(platformUrl string, index commandsummary.In
 		// Not running in Github no need to add analytics
 		return platformUrl
 	}
-	suffixValues := []string{fmt.Sprintf("gh_job_id=%s", url.PathEscape(githubJobId))}
+	// M=3 (type of event)
+	suffixValues := []string{"s=1", "m=3", fmt.Sprintf("gh_job_id=%s", url.PathEscape(githubJobId))}
 	// Add section analytics
 	indexValue := "gh_section="
 	switch index {
