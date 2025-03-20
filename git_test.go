7fea060dfc98b89f81dc287033485c05651c3f21
attiasas <assafattias93@gmail.com>
2025-03-20 16:12:10 +0200

    fix

--- git_test.go
+++ git_test.go
@@ -197,7 +197,6 @@ func TestXrayAuditJasSkipNotApplicableCvesViolations(t *testing.T) {
 			Violations: &validations.ViolationCount{
 				ValidateScan:                &validations.ScanCount{Sca: 8, Sast: 2, Secrets: 2},
 				ValidateApplicabilityStatus: &validations.ApplicabilityStatusCount{NotApplicable: 3, NotCovered: 5},
-				ValidateApplicabilityStatus: &validations.ApplicabilityStatusCount{NotApplicable: 3, NotCovered: 5},
 			},
 			ExactResultsMatch: true,
 		},
