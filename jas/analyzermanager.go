1a312b3d25275f7a5a979eec9cf03b6a7c4df3e4
attiasas <assafattias93@gmail.com>
2025-05-27 14:54:53 +0300

    revert am to 1.19.2

--- jas/analyzermanager.go
+++ jas/analyzermanager.go
@@ -25,7 +25,7 @@ import (
 const (
 	ApplicabilityFeatureId                    = "contextual_analysis"
 	AnalyzerManagerZipName                    = "analyzerManager.zip"
-	defaultAnalyzerManagerVersion             = "1.20.1"
+	defaultAnalyzerManagerVersion             = "1.19.2"
 	analyzerManagerDownloadPath               = "xsc-gen-exe-analyzer-manager-local/v1"
 	analyzerManagerDirName                    = "analyzerManager"
 	analyzerManagerExecutableName             = "analyzerManager"
