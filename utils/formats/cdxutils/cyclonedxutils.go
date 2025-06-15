e3125b84ec5ee8461b9d018fa400d46e3cf51346
attiasas <assafattias93@gmail.com>
2025-06-15 18:19:43 +0300

    implement cdx utils for for-each sbom component

--- utils/formats/cdxutils/cyclonedxutils.go
+++ utils/formats/cdxutils/cyclonedxutils.go
@@ -1,7 +1,13 @@
 package cdxutils
 
-import "github.com/CycloneDX/cyclonedx-go"
+import (
+	"github.com/CycloneDX/cyclonedx-go"
 
+	"github.com/jfrog/gofrog/datastructures"
+)
+
+// AppendProperties appends new properties to the existing properties list.
+// Returns the updated properties list.
 func AppendProperties(properties *[]cyclonedx.Property, newProperties ...cyclonedx.Property) *[]cyclonedx.Property {
 	for _, property := range newProperties {
 		// Check if the property already exists
@@ -18,6 +24,7 @@ func AppendProperties(properties *[]cyclonedx.Property, newProperties ...cyclone
 	return properties
 }
 
+// SearchProperty searches for a property by name in the provided properties list.
 func SearchProperty(properties *[]cyclonedx.Property, name string) *cyclonedx.Property {
 	if properties == nil || len(*properties) == 0 {
 		return nil
@@ -28,4 +35,72 @@ func SearchProperty(properties *[]cyclonedx.Property, name string) *cyclonedx.Pr
 		}
 	}
 	return nil
+}
+
+// SearchDependencyEntry searches for a dependency entry by reference in the provided dependencies list.
+func SearchDependencyEntry(dependencies *[]cyclonedx.Dependency, ref string) *cyclonedx.Dependency {
+	if dependencies == nil || len(*dependencies) == 0 {
+		return nil
+	}
+	for _, dependency := range *dependencies {
+		if dependency.Ref == ref {
+			return &dependency
+		}
+	}
+	return nil
+}
+
+// IsDirectDependency checks if a component is a direct dependency of a root component in the provided components and dependencies.
+func IsDirectDependency(dependencies *[]cyclonedx.Dependency, ref string) bool {
+	if dependencies == nil || len(*dependencies) == 0 {
+		return false
+	}
+	for _, root := range GetRootDependenciesEntries(dependencies) {
+		if root.Ref == ref {
+			// The component is a root, hence it is not a direct dependency
+			return false
+		}
+		if root.Dependencies == nil || len(*root.Dependencies) == 0 {
+			// No dependencies, continue to the next root
+			continue
+		}
+		for _, dep := range *root.Dependencies {
+			if dep == ref {
+				// The component is a direct dependency of this root
+				return true
+			}
+		}
+	}
+	// No direct dependency found
+	return false
+}
+
+func GetRootDependenciesEntries(dependencies *[]cyclonedx.Dependency) (roots []cyclonedx.Dependency) {
+	roots = []cyclonedx.Dependency{}
+	if dependencies == nil || len(*dependencies) == 0 {
+		// If no dependencies are found, return an empty list
+		return
+	}
+	// Create a Set to track all references that are listed in `dependsOn`
+	refs := datastructures.MakeSet[string]()
+	dependedRefs := datastructures.MakeSet[string]()
+	// Populate the maps
+	for _, dep := range *dependencies {
+		if dep.Ref == "" || dep.Dependencies == nil {
+			// No dependencies, continue
+			continue
+		}
+		refs.Add(dep.Ref)
+		for _, dependsOn := range *dep.Dependencies {
+			dependedRefs.Add(dependsOn)
+		}
+	}
+	// Identify root dependencies (those not listed in any `dependsOn`)
+	for _, id := range refs.ToSlice() {
+		if dep := SearchDependencyEntry(dependencies, id); dep != nil && !dependedRefs.Exists(dep.Ref) {
+			// This is a root dependency, add it
+			roots = append(roots, *dep)
+		}
+	}
+	return
 }
