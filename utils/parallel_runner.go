3757ea18ea6d4177c67a2fae561c1703e9c9956e
attiasas <assafattias93@gmail.com>
2025-06-16 11:03:18 +0300

    Encapsulate logic in parallel runner

--- utils/parallel_runner.go
+++ utils/parallel_runner.go
@@ -11,6 +11,8 @@ type SecurityParallelRunner struct {
 	ScaScansWg    sync.WaitGroup // Verify that the sca scan routines are done before running contextual scan
 	JasScannersWg sync.WaitGroup // Verify that all scanners routines are done before cleaning temp dir
 	JasWg         sync.WaitGroup // Verify that downloading analyzer manager and running all scanners are done
+
+	onScanEndFunc func()
 }
 
 func NewSecurityParallelRunner(numOfParallelScans int) SecurityParallelRunner {
@@ -20,4 +22,22 @@ func NewSecurityParallelRunner(numOfParallelScans int) SecurityParallelRunner {
 func CreateSecurityParallelRunner(numOfParallelScans int) *SecurityParallelRunner {
 	securityParallelRunner := NewSecurityParallelRunner(numOfParallelScans)
 	return &securityParallelRunner
+}
+
+func (spr *SecurityParallelRunner) OnScanEnd(funcToRunOnScanEnd func()) *SecurityParallelRunner {
+	spr.onScanEndFunc = funcToRunOnScanEnd
+	return spr
+}
+
+func (spr *SecurityParallelRunner) Start() {
+	go func() {
+		spr.ScaScansWg.Wait()
+		spr.JasWg.Wait()
+		spr.JasScannersWg.Wait()
+		if spr.onScanEndFunc != nil {
+			spr.onScanEndFunc()
+		}
+		spr.Runner.Done()
+	}()
+	spr.Runner.Run()
 }
