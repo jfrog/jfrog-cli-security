afc626424c89de66ffaaa90c6bc14cf36b98703b
attiasas <assafattias93@gmail.com>
2025-06-17 17:08:40 +0300

    update interface

--- sca/scan/scascan.go
+++ sca/scan/scascan.go
@@ -8,10 +8,13 @@ import (
 
 // SbomScanStrategy is an interface for scanning SBOMs using different strategies.
 type SbomScanStrategy interface {
-	// Parallel creates new instance of Scanner for parallel execution.
+	// SetThread sets the thread ID for the scan strategy.
 	SetThread(threadId int) SbomScanStrategy
-	// ScaScanTask scans the given SBOM using the specified technology.
+	// DeprecatedScanTask scans the given SBOM using the specified technology returning the scan response.
+	// This method is deprecated and only used for backward compatibility until the new BOM can contain all the information scanResponse contains.
+	// Missing attributes:
+	// - ExtendedInformation (JfrogResearchInformation): ShortDescription, FullDescription, frogResearchSeverityReasons, Remediation
 	DeprecatedScanTask(target *cyclonedx.BOM) (services.ScanResponse, error)
-	// Perform a Scan on the given SBOM and return the CycloneDX BOM.
+	// Perform a Scan on the given SBOM and return the enriched CycloneDX BOM and violations.
 	SbomEnrichTask(target *cyclonedx.BOM) (*cyclonedx.BOM, []services.Violation, error)
 }
