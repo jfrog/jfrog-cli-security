fbdf22d9990f18ad5bef62e33746749f95b29a39
attiasas <assafattias93@gmail.com>
2025-06-26 19:42:04 +0300

    add scang config

---
+++ sca/bom/scang/config.go
@@ -0,0 +1,15 @@
+package scang
+
+// Config holds the configuration for scang library options.
+type Config struct {
+	// The BOMRef of the scanned target, will be used at the Metadata and considered the Root.
+	BomRef string `json:"bom-ref,omitempty"`
+	// The component type of the target ("application" / "library" / "file"...), will be used at the Metadata component.
+	Type string `json:"type,omitempty"`
+	// The name of the target, will be used at the Metadata component.
+	Name string `json:"name,omitempty"`
+	// [Optional] The version of the target, will be used at the Metadata component.
+	Version string `json:"version,omitempty"`
+	// [Optional] Patterns (git ignore like) to ignore when scanning the target.
+	IgnorePatterns []string `json:"ignorePatterns,omitempty"`
+}
