8eda01563e957b5ff9c1a5f6adbedba03ba8fc6c
attiasas <assafattias93@gmail.com>
2025-07-02 17:59:58 +0300

    try fix

--- utils/paths.go
+++ utils/paths.go
@@ -145,7 +145,7 @@ func ToURI(path string) string {
 	// If it's a Windows path, prepend "file:///" and replace the drive letter
 	if len(path) > 2 && path[1] == ':' {
 		// Convert "C:\\path\\to\\file" to "file:///C:/path/to/file"
-		path = "file:///" + path[:2] + path[2:]
+		path = "file://" + path[:2] + path[2:]
 	} else {
 		// For Linux/Unix or other paths, just add "file://"
 		path = "file://" + path
