ba4cf451d95fd4febf547a1e76e95239debcf342
attiasas <assafattias93@gmail.com>
2025-07-09 22:52:45 +0300

    update GetRelation with option to not skip dummy root for exclude

--- utils/formats/cdxutils/cyclonedxutils_test.go
+++ utils/formats/cdxutils/cyclonedxutils_test.go
@@ -388,7 +388,7 @@ func TestGetComponentRelation(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result := GetComponentRelation(tt.bom, tt.componentRef)
+			result := GetComponentRelation(tt.bom, tt.componentRef, true)
 			assert.Equal(t, tt.expected, result, "Expected component relation does not match")
 		})
 	}
