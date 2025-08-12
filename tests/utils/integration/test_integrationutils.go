e1b3fe6e6908686ee2b2d2fd725700de7e6c2ecb
attiasas <assafattias93@gmail.com>
2025-08-12 15:17:01 +0300

    add option to skip tests for amount of days

--- tests/utils/integration/test_integrationutils.go
+++ tests/utils/integration/test_integrationutils.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -36,6 +37,22 @@ import (
 
 func getSkipTestMsg(testName, testFlag string) string {
 	return fmt.Sprintf("Skipping %s tests. To run them, add the '%s=true' option or don't supply any options.", testName, testFlag)
+}
+
+// SkipTestIfDurationNotPassed skips the test if the specified duration in days hasn't passed since the given date.
+// dateStr should be in the format "02-01-2006" (DD-MM-YYYY).
+// durationDays is the number of days that should have passed.
+func SkipTestIfDurationNotPassed(t *testing.T, dateStr string, durationDays int) {
+	givenDate, err := time.Parse("02-01-2006", dateStr)
+	if err != nil {
+		t.Fatalf("Invalid date format '%s'. Expected format: DD-MM-YYYY", dateStr)
+	}
+
+	daysSinceDate := int(time.Since(givenDate).Hours() / 24)
+	if daysSinceDate < durationDays {
+		t.Skipf("Skipping test. Only %d days have passed since %s, but %d days are required.",
+			daysSinceDate, dateStr, durationDays)
+	}
 }
 
 func InitUnitTest(t *testing.T) {
