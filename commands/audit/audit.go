47c85c3aa01a4e9a4e51057daeac5563409133f7
attiasas <assafattias93@gmail.com>
2025-08-12 15:05:47 +0300

    only show note when no git repo resource and violations

--- commands/audit/audit.go
+++ commands/audit/audit.go
@@ -221,7 +221,7 @@ func (auditCmd *AuditCommand) getResultWriter(cmdResults *results.SecurityComman
 	var messages []string
 	if !cmdResults.EntitledForJas {
 		messages = []string{coreutils.PrintTitle("The ‘jf audit’ command also supports JFrog Advanced Security features, such as 'Contextual Analysis', 'Secrets Detection', 'IaC Scan' and ‘SAST’.\nThis feature isn't enabled on your system. Read more - ") + coreutils.PrintLink(utils.JasInfoURL)}
-	} else if cmdResults.HasViolationContext() {
+	} else if cmdResults.HasViolationContext() && len(cmdResults.ResultContext.GitRepoHttpsCloneUrl) == 0 {
 		messages = []string{"Note: The following types of violations are NOT supported by this audit:\n- Secrets\n- Infrastructure as Code (IaC)\n- Static Application Security Testing (SAST)"}
 	}
 	return output.NewResultsWriter(cmdResults).
