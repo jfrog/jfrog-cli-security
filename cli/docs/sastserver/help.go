f69c300b5df92e33683f654e01363a5b8a61f371
Iddo Hauschner <iddoh@jfrog.com>
2026-01-26 11:03:39 +0200

    move functions to common use

--- cli/docs/sastserver/help.go
+++ cli/docs/sastserver/help.go
@@ -8,10 +8,6 @@ func GetDescription() string {
 	return "Runs a local source code analysis as a local SAST server, allowing access to tools which reflect source code analysis"
 }
 
-func GetArguments() []components.Argument {
-	return []components.Argument{}
-}
-
 func GetFlags() []components.Flag {
 	return []components.Flag{
 		components.NewStringFlag("port", "Specifies the port to run the SAST server on."),
--- commands/source_mcp/source_mcp.go
+++ commands/source_mcp/source_mcp.go
@@ -3,20 +3,15 @@ package source_mcp
 import (
 	"fmt"
 	"io"
-	"os/exec"
-	"time"
 
 	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
 	"github.com/jfrog/jfrog-cli-security/jas"
-	"github.com/jfrog/jfrog-cli-security/utils"
 	"github.com/jfrog/jfrog-cli-security/utils/xray"
-
-	"github.com/jfrog/jfrog-client-go/utils/log"
 )
 
 const (
 	cmd            = "mcp-sast"
-	McpEntitlement = "local_sast_mcp"
+	mcpEntitlement = "local_sast_mcp"
 )
 
 type McpCommand struct {
@@ -27,117 +22,8 @@ type McpCommand struct {
 	ErrorPipe     io.Writer
 }
 
-func establishPipeToFile(dst io.WriteCloser, src io.Reader) {
-	defer func() {
-		if err := dst.Close(); err != nil {
-			log.Error(fmt.Sprintf("Error closing pipe: %v", err))
-		}
-	}()
-	_, err := io.Copy(dst, src)
-	if err != nil {
-		log.Error("Error establishing pipe")
-	}
-}
-
-func establishPipeFromFile(dst io.Writer, src io.ReadCloser) {
-	defer func() {
-		if err := src.Close(); err != nil {
-			log.Error(fmt.Sprintf("Error closing pipe: %v", err))
-		}
-	}()
-	_, err := io.Copy(dst, src)
-	if err != nil {
-		log.Error("Error establishing pipe")
-	}
-}
-
-func RunAmMcpWithPipes(env map[string]string, cmd string, input_pipe io.Reader, output_pipe io.Writer, error_pipe io.Writer, timeout int, args ...string) error {
-	am_path, err := jas.GetAnalyzerManagerExecutable()
-	if err != nil {
-		return err
-	}
-
-	allArgs := append([]string{cmd}, args...)
-	log.Info(fmt.Sprintf("Launching: %s; command %s; arguments %v", am_path, cmd, args))
-	command := exec.Command(am_path, allArgs...)
-	command.Env = utils.ToCommandEnvVars(env)
-
-	stdin, _error := command.StdinPipe()
-	if _error != nil {
-		log.Error(fmt.Sprintf("Error creating MCPService stdin pipe: %v", _error))
-		return _error
-	}
-	defer func() {
-		if err := stdin.Close(); err != nil {
-			log.Error(fmt.Sprintf("Error closing stdin pipe: %v", err))
-		}
-	}()
-
-	stdout, _error := command.StdoutPipe()
-	if _error != nil {
-		log.Error(fmt.Sprintf("Error creating MCPService stdout pipe: %v", _error))
-		return _error
-	}
-	defer func() {
-		if err := stdout.Close(); err != nil {
-			log.Error(fmt.Sprintf("Error closing stdout pipe: %v", err))
-		}
-	}()
-
-	stderr, _error := command.StderrPipe()
-	if _error != nil {
-		log.Error(fmt.Sprintf("Error creating MCPService stderr pipe: %v", _error))
-		return _error
-	}
-	defer func() {
-		if err := stderr.Close(); err != nil {
-			log.Error(fmt.Sprintf("Error closing stderr pipe: %v", err))
-		}
-	}()
-
-	go establishPipeToFile(stdin, input_pipe)
-	go establishPipeFromFile(error_pipe, stderr)
-	go establishPipeFromFile(output_pipe, stdout)
-
-	if _error := command.Start(); _error != nil {
-		log.Error(fmt.Sprintf("Error starting MCPService subprocess: %v", _error))
-		return _error
-	}
-
-	if timeout > 0 {
-		waitCh := make(chan error, 1)
-		go func() {
-			waitCh <- command.Wait()
-		}()
-		select {
-		case _error = <-waitCh:
-		case <-time.After(time.Duration(timeout) * time.Second):
-			log.Warn("Timeout reached")
-
-			return nil
-		}
-	} else {
-		_error = command.Wait()
-	}
-
-	if _error != nil {
-		log.Error(fmt.Sprintf("Error waiting for MCPService subprocess: %v", _error))
-		return _error
-	}
-	return nil
-}
-
 func (mcpCmd *McpCommand) runWithTimeout(timeout int, cmd string, envVars map[string]string) (err error) {
-	return RunAmWithPipesAndTimeout(envVars, cmd, mcpCmd.InputPipe, mcpCmd.OutputPipe, mcpCmd.ErrorPipe, timeout, mcpCmd.Arguments...)
-}
-
-// RunAmWithPipesAndTimeout downloads the analyzer manager if needed and runs the command with pipes
-func RunAmWithPipesAndTimeout(envVars map[string]string, cmd string, inputPipe io.Reader, outputPipe io.Writer, errorPipe io.Writer, timeout int, args ...string) error {
-	err := jas.DownloadAnalyzerManagerIfNeeded(0)
-	if err != nil {
-		log.Error(fmt.Sprintf("Failed to download Analyzer Manager: %v", err))
-	}
-	return RunAmMcpWithPipes(envVars, cmd, inputPipe, outputPipe, errorPipe, timeout, args...)
+	return jas.RunAnalyzerManagerWithPipesAndDownload(envVars, cmd, mcpCmd.InputPipe, mcpCmd.OutputPipe, mcpCmd.ErrorPipe, timeout, mcpCmd.Arguments...)
 }
 
 func (mcpCmd *McpCommand) Run() (err error) {
@@ -145,7 +31,7 @@ func (mcpCmd *McpCommand) Run() (err error) {
 	if err != nil {
 		return err
 	}
-	if entitled, err := IsEntitledForSourceMCP(mcpCmd.ServerDetails); err != nil {
+	if entitled, err := isEntitledForSourceMCP(mcpCmd.ServerDetails); err != nil {
 		return err
 	} else if !entitled {
 		return fmt.Errorf("it appears your current license doesn't include this feature.\nTo enable this functionality, an upgraded license is required. Please contact your JFrog representative for more details")
@@ -153,8 +39,7 @@ func (mcpCmd *McpCommand) Run() (err error) {
 	return mcpCmd.runWithTimeout(0, cmd, am_env)
 }
 
-// IsEntitledForSourceMCP checks if the user is entitled to use the local SAST MCP feature
-func IsEntitledForSourceMCP(serverDetails *config.ServerDetails) (entitled bool, err error) {
+func isEntitledForSourceMCP(serverDetails *config.ServerDetails) (entitled bool, err error) {
 	xrayManager, err := xray.CreateXrayServiceManager(serverDetails)
 	if err != nil {
 		return
@@ -163,5 +48,5 @@ func IsEntitledForSourceMCP(serverDetails *config.ServerDetails) (entitled bool,
 	if err != nil {
 		return
 	}
-	return xray.IsEntitled(xrayManager, xrayVersion, McpEntitlement)
+	return xray.IsEntitled(xrayManager, xrayVersion, mcpEntitlement)
 }
