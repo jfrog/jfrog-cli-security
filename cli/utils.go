a640e510badd3e10e1a377956640afa2459151e3
attiasas <assafattias93@gmail.com>
2026-02-16 15:22:07 +0200

    fix static

--- cli/utils.go
+++ cli/utils.go
@@ -138,7 +138,7 @@ func validateSnippetDetection(c *components.Context) (bool, error) {
 		return false, err
 	}
 	// Make sure SCA is requested or SBOM is requested
-	if !(utils.IsScanRequested(utils.SourceCode, utils.ScaScan, subScans...) || c.GetBoolFlagValue(flags.Sbom)) {
+	if !utils.IsScanRequested(utils.SourceCode, utils.ScaScan, subScans...) && !c.GetBoolFlagValue(flags.Sbom) {
 		return false, errorutils.CheckErrorf("Snippet detection is only supported when SCA is requested or SBOM is requested")
 	}
 	return true, nil
