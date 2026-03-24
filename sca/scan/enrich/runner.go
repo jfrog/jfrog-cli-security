03ad16c08638d9b525ebc1b8e39020ccc1df7485
attiasas <assafattias93@gmail.com>
2026-03-24 11:57:46 +0200

    Convert remediation error to not fail commands

--- sca/scan/enrich/runner.go
+++ sca/scan/enrich/runner.go
@@ -19,6 +19,8 @@ import (
 type EnrichScanStrategy struct {
 	serverDetails *config.ServerDetails
 	projectKey    string
+	// Fix version attachment is not critical, so we continue even if it fails without returning an error
+	failOnRemediationError bool
 }
 
 func NewEnrichScanStrategy() *EnrichScanStrategy {
@@ -39,6 +41,14 @@ func (ess *EnrichScanStrategy) WithOptions(options ...scan.SbomScanOption) scan.
 		option(ess)
 	}
 	return ess
+}
+
+func WithFailOnRemediationError(failOnRemediationError bool) scan.SbomScanOption {
+	return func(sss scan.SbomScanStrategy) {
+		if ess, ok := sss.(*EnrichScanStrategy); ok {
+			ess.failOnRemediationError = failOnRemediationError
+		}
+	}
 }
 
 func (ess *EnrichScanStrategy) PrepareStrategy() (err error) {
@@ -65,16 +75,27 @@ func (ess *EnrichScanStrategy) SbomEnrichTask(target *cyclonedx.BOM) (enriched *
 	}
 	log.Debug("SBOM enrichment completed successfully")
 	// Fixed versions are not returned from the enrich API, next we need to enrich with remediation API.
+	if e := ess.attachFixedVersionsToVulnerabilities(enriched); e != nil {
+		e = fmt.Errorf("failed to enrich SBOM with remediation: %w", e)
+		if ess.failOnRemediationError {
+			return enriched, e
+		}
+		log.Error(e.Error())
+	} else {
+		log.Debug("SBOM remediation enrichment completed successfully")
+	}
+	return enriched, nil
+}
+
+func (ess *EnrichScanStrategy) attachFixedVersionsToVulnerabilities(bom *cyclonedx.BOM) error {
 	xrayManager, err := xray.CreateXrayServiceManager(ess.serverDetails, xray.WithScopedProjectKey(ess.projectKey))
 	if err != nil {
-		return enriched, fmt.Errorf("failed to create Xray service manager: %w", err)
+		return fmt.Errorf("failed to create Xray service manager: %w", err)
 	}
-	err = remediation.AttachFixedVersionsToVulnerabilities(xrayManager, enriched)
-	if err != nil {
-		return enriched, fmt.Errorf("failed to attach fixed versions to vulnerabilities: %w", err)
+	if e := remediation.AttachFixedVersionsToVulnerabilities(xrayManager, bom); e != nil {
+		return fmt.Errorf("failed to attach fixed versions to vulnerabilities: %w", e)
 	}
-	log.Debug("SBOM remediation enrichment completed successfully")
-	return
+	return nil
 }
 
 func (ess *EnrichScanStrategy) DeprecatedScanTask(target *cyclonedx.BOM) (techResults services.ScanResponse, err error) {
