12dbd616dd42f1960ad8c896f91680b71fa81f8d
attiasas <assafattias93@gmail.com>
2026-04-26 11:51:40 +0300

    Only warn when components are empty at parsing

--- utils/results/common.go
+++ utils/results/common.go
@@ -243,6 +243,7 @@ func ForEachSbomComponent(bom *cyclonedx.BOM, handler ParseSbomComponentFunc) (e
 
 func SplitComponents(target string, impactedPackages map[string]services.Component) (impactedPackagesIds []string, fixedVersions [][]string, directComponents [][]formats.ComponentRow, impactPaths [][][]formats.ComponentRow, err error) {
 	if len(impactedPackages) == 0 {
+		log.Warn(fmt.Sprintf("Failed while parsing the response from Xray: components map is empty for target '%s'", target))
 		err = errorutils.CheckErrorf("failed while parsing the response from Xray: components map is empty")
 		return
 	}
