cf1c9b9c1f598bbf59bea28505a2a28c87d66660
attiasas <assafattias93@gmail.com>
2026-05-25 10:02:05 +0300

    Fix tests with 2 components in cve

--- git_test.go
+++ git_test.go
@@ -298,8 +298,8 @@ func TestGitAuditJasSkipNotApplicableCvesViolations(t *testing.T) {
 		xrayVersion, xscVersion, "",
 		validations.ValidationParams{
 			Violations: &validations.ViolationCount{
-				ValidateScan:                &validations.ScanCount{Sca: 19, Sast: 2, Secrets: 2},
-				ValidateApplicabilityStatus: &validations.ApplicabilityStatusCount{NotApplicable: 15, NotCovered: 4, Inactive: 2},
+				ValidateScan:                &validations.ScanCount{Sca: 20, Sast: 2, Secrets: 2},
+				ValidateApplicabilityStatus: &validations.ApplicabilityStatusCount{NotApplicable: 14, NotCovered: 6, Inactive: 2},
 			},
 			ExactResultsMatch: true,
 		},
